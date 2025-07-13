e16b016ef10c2b5aa619044946f3f3f2a71c3d9c
najmuschy <najmus.chy.12@gmai.com>
2025-07-13 16:41:57 +0600

    user auth completed

---
+++ lib/ui/screens/add_new_task_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:ostad_flutter_batch_nine/data/service/network_client.dart';
+import 'package:ostad_flutter_batch_nine/data/utils/urls.dart';
+import 'package:ostad_flutter_batch_nine/ui/widgets/centered_circular_progress_indicator.dart';
+import 'package:ostad_flutter_batch_nine/ui/widgets/screen_background.dart';
+import 'package:ostad_flutter_batch_nine/ui/widgets/snack_bar_message.dart';
+import 'package:ostad_flutter_batch_nine/ui/widgets/tm_app_bar.dart';
+
+class AddNewTaskScreen extends StatefulWidget {
+  const AddNewTaskScreen({super.key});
+
+  @override
+  State<AddNewTaskScreen> createState() => _AddNewTaskScreenState();
+}
+
+class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
+  final TextEditingController _titleTEController = TextEditingController();
+  final TextEditingController _descriptionTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _addNewTaskInProgress = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const TMAppBar(),
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 32),
+                  Text(
+                    'Add New Task',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    controller: _titleTEController,
+                    textInputAction: TextInputAction.next,
+                    decoration: const InputDecoration(
+                      hintText: 'Title'
+                    ),
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your title';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    maxLines: 6,
+                    controller: _descriptionTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'Description',
+                      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+                    ),
+                    validator: (String? value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your Description';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Visibility(
+                    visible: _addNewTaskInProgress == false,
+                    replacement: const CenteredCircularProgressIndicator(),
+                    child: ElevatedButton(
+                      onPressed: _onTapSubmitButton,
+                      child: const Icon(Icons.arrow_circle_right_outlined),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTapSubmitButton() {
+    if (_formKey.currentState!.validate()) {
+      _addNewTask();
+    }
+  }
+
+  Future<void> _addNewTask() async {
+    _addNewTaskInProgress = true;
+    setState(() {});
+
+    Map<String, dynamic> requestBody = {
+      "title": _titleTEController.text.trim(),
+      "description": _descriptionTEController.text.trim(),
+      "status": "New"
+    };
+
+    final NetworkResponse response = await NetworkClient.postRequest(
+      url: Urls.createTaskUrl,
+      body: requestBody,
+    );
+
+    _addNewTaskInProgress = false;
+    setState(() {});
+
+    if (response.isSuccess) {
+      _clearTextFields();
+      showSnackBarMessage(context, 'New task added!');
+    } else {
+      showSnackBarMessage(context, response.errorMessage);
+    }
+  }
+
+  void _clearTextFields() {
+    _titleTEController.clear();
+    _descriptionTEController.clear();
+  }
+
+  @override
+  void dispose() {
+    _titleTEController.dispose();
+    _descriptionTEController.dispose();
+    super.dispose();
+  }
+}
